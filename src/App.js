8811a2619092b13ca1e8aa705077583490ba832f
Flying-Developer <kopasd99@gmail.com>
2022-05-06 20:36:16 +0900

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
+import React from 'react';
 import './styles.css';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2> hello </h2>
-      <div></div>
-      <h2>Start editing to see some magic happen!</h2>
+      <header id="Header">Header</header>
+      <main id="Main">
+        <article id="Article"> Article</article>
+        <nav id="Nav">Nav</nav>
+        <footer id="Footer">Footer</footer>
+      </main>
+      <aside id="Side_left">left</aside>
+      <aside id="Side_right">right</aside>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 
-import App from "./App";
-
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
